99f7c6764d5637a4d2420d7c02570b7d5f375b9f
Kundan-404 <kundanmalhi45@gmail.com>
2023-09-24 19:20:52 +0500

    initial

---
+++ a.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const res = require('express/lib/response')
+const app = express()
+const port = 3000
+
+
+app.get('/', (req,res) => {
+    res.send('Hello World');
+})
+
+app.listen(port, () =>{
+    console.log("App listening from port ${port}")
+})

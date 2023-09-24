699cbbe663b2966f963e4f5a163605fb9a7ae713
Kundan-404 <kundanmalhi45@gmail.com>
2023-09-24 21:50:52 +0500

    2nd commit

--- a.js
+++ a.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const res = require('express/lib/response')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 
@@ -8,6 +9,23 @@ app.get('/', (req,res) => {
     res.send('Hello World');
 })
 
+app.get('/blog', (req,res) => {
+    res.send('Hello World My blog');
+})
+
+app.post("/recipe", (req, res)=>{
+    console.log(req.body);
+    res.send(req.body);
+})
+
 app.listen(port, () =>{
     console.log("App listening from port ${port}")
+})
+
+mongoose.
+connect("mongodb+srv://admin:123@crud-api.5rgdwol.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp")
+.then(() => {
+    console.log("Successfully Connected")
+}).catch(() =>{
+    console.log("Not connected. Some error")
 })
